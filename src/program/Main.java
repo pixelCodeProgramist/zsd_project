99f97f9d80dcd0c746c5c94e1a60cbe0e66c3d4a
Arcmsitrz <adsa2222@interia.pl>
2021-10-23 18:41:10 +0200

    Added another sort algorithm called radix sort

--- src/program/Main.java
+++ src/program/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 	    List<Integer> integerList = Arrays.asList(12,-1,-5,90,21,0,20,21,-100,500,2,-1);
 		integerList = new ArrayList(integerList);
-	    Sorter cSorter = new BucketSorter(integerList);
+	    Sorter cSorter = new CoutingSorter(integerList);
 	    cSorter.sort();
     }
 }
---
+++ src/program/RadixSorter.java
@@ -0,0 +1,92 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadixSorter implements Sorter {
+    private List<Integer> array;
+    private List<Integer> negative;
+    private List<Integer> positive;
+
+    public RadixSorter(List<Integer> array) {
+        this.array = array;
+        negative = new ArrayList<>();
+        positive = new ArrayList<>();
+    }
+
+    @Override
+    public void sort() {
+        long time = System.currentTimeMillis();
+        int max = array.get(0);
+        int min = array.get(0);
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) > 0) positive.add(array.get(i));
+            else negative.add(array.get(i));
+            if (array.get(i) > max) max = array.get(i);
+            if (array.get(i) < min) min = array.get(i);
+        }
+        List<Integer> tempArray = new ArrayList<>(array);
+        if (negative.size() > 0 && positive.size() > 0) sortPositiveAndNegativeNumbers(max, min , tempArray);
+        else if (positive.size() == 0) sortNegativeNumbers(min, tempArray);
+        else sortPositiveNumbers(max, tempArray);
+        System.out.println("Sort time = " + (System.currentTimeMillis() - time) + " ms");
+        System.out.println(array);
+    }
+
+    private void sortPositiveAndNegativeNumbers(int max, int min, List<Integer> tempArray) {
+        max = max > Math.abs(min) ? max : min;
+        for (int power = 1; max / power > 0; power *= 10) {
+            countingPositiveNumberSort(positive, tempArray, power);
+            countingNegativeNumberSort(negative, tempArray, power);
+        }
+        array.clear();
+        array.addAll(negative);
+        array.addAll(positive);
+    }
+
+    private void sortNegativeNumbers(int min, List<Integer> tempArray) {
+        for (int power = 1; min / power > 0; power *= 10)
+            countingNegativeNumberSort(negative, tempArray, power);
+        array.clear();
+        array.addAll(negative);
+    }
+
+    private void sortPositiveNumbers(int max, List<Integer> tempArray) {
+        for (int power = 1; max / power > 0; power *= 10)
+            countingPositiveNumberSort(positive, tempArray, power);
+        array.clear();
+        array.addAll(positive);
+    }
+
+    private void countingNegativeNumberSort(List<Integer> array, List<Integer> tempArray, int power) {
+        int[] counters = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < array.size(); i++)
+            counters[9 - (Math.abs(array.get(i)) / power) % 10]++;
+        for (int i = 1; i < 10; i++)
+            counters[i] += counters[i - 1];
+        int position;
+        for (int i = array.size() - 1; i >= 0; i--) {
+            position = 9 - (Math.abs(array.get(i)) / power) % 10;
+            tempArray.set(counters[position] - 1, array.get(i));
+            counters[position]--;
+        }
+        for (int i = 0; i < array.size(); i++)
+            array.set(i, tempArray.get(i));
+    }
+
+    private void countingPositiveNumberSort(List<Integer> array, List<Integer> tempArray, int power) {
+        int[] counters = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < array.size(); i++)
+            counters[(Math.abs(array.get(i)) / power) % 10]++;
+        for (int i = 1; i < 10; i++)
+            counters[i] += counters[i - 1];
+        int position;
+        for (int i = array.size() - 1; i >= 0; i--) {
+            position = (Math.abs(array.get(i)) / power) % 10;
+            tempArray.set(counters[position] - 1, array.get(i));
+            counters[position]--;
+        }
+        for (int i = 0; i < array.size(); i++)
+            array.set(i, tempArray.get(i));
+    }
+}
